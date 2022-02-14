2b13a5f4ad1952cdc2184551329e556ba27fcb16
mairabassetto <mairabassetto@gmail.com>
2022-02-14 09:12:24 -0300

    Challenge concluded. :tada:

--- src/components/Slides.tsx
+++ src/components/Slides.tsx
@@ -2,8 +2,50 @@ import { Flex, Heading, Link, Text } from "@chakra-ui/react";
 
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Keyboard, Pagination, Navigation } from "swiper";
+import 'swiper/css';
+import 'swiper/css/virtual';
+import { useState } from "react";
 
 export function Slides() {
+  const [slides, setSlides] = useState([
+    {
+      href: '/continents/europe',
+      figure: '/images/europe.jpg',
+      title: 'Europa',
+      description: 'O continente mais antigo.'
+    },
+    {
+      href: '/continents/asia',
+      figure: '/images/asia.jpg',
+      title: 'Ásia',
+      description: 'O continente mais tecnológico.'
+    },
+    {
+      href: '/continents/africa',
+      figure: '/images/africa.jpg',
+      title: 'África',
+      description: 'O continente mais selvagem.'
+    },
+    {
+      href: '/continents/southamerica',
+      figure: '/images/southamerica.jpg',
+      title: 'América do Sul',
+      description: 'O continente mais diversificado.'
+    },
+    {
+      href: '/continents/northamerica',
+      figure: '/images/northamerica.jpg',
+      title: 'América do Norte',
+      description: 'The "new world".'
+    },
+    {
+      href: '/continents/oceania',
+      figure: '/images/oceania.jpg',
+      title: 'Oceania',
+      description: 'O menor continente do mundo.'
+    },
+  ]);
+  
   return (
     <Flex
         w='100%'
@@ -26,75 +68,31 @@ export function Slides() {
           navigation={true}
           modules={[Keyboard, Pagination, Navigation]}
           className="mySwiper"
+
         >
-          <SwiperSlide>
-            <Flex 
-              w='100%'
-              h='100%'
-              align='center' 
-              justify='center' 
-              bgImage='url(/images/europe.jpg)'
-              bgRepeat='no-repeat'
-              bgPosition='center'
-              bgSize='cover'
-              textAlign='center'
-              >
-              <Link href={'/continents/europe'}>
-                <a>
-                  <Heading 
-                    fontWeight='700' 
-                    fontSize={['2xl', '3xl', '4xl', '5xl']} 
-                    color='gray.50'
-                  > 
-                    Europe 
-                  </Heading>
-
-                  <Text 
-                    fontWeight='700' 
-                    fontSize={['sm', 'lg', 'xl', '2xl']} 
-                    color='gray.50'
-                  > 
-                    O continente mais antigo. 
-                  </Text>
-                </a>
-              </Link>
-            </Flex>
-          </SwiperSlide>
-
-          <SwiperSlide>
-            <Flex 
-              w='100%'
-              h='100%'
-              align='center' 
-              justify='center' 
-              bgImage='url(/images/asia.jpg)'
-              bgRepeat='no-repeat'
-              bgPosition='center'
-              bgSize='cover'
-              textAlign='center'
-            >
-              <Link href={'/continents/asia'}>
-                <a>
-                  <Heading 
-                    fontWeight='700' 
-                    fontSize={['2xl', '3xl', '4xl', '5xl']} 
-                    color='gray.50'
-                  > 
-                    Ásia 
-                  </Heading>
-
-                  <Text 
-                    fontWeight='700' 
-                    fontSize={['sm', 'lg', 'xl', '2xl']} 
-                    color='gray.50'
-                  > 
-                    O continente mais tecnológico. 
-                  </Text>
-                </a>
-              </Link>
-            </Flex>
-          </SwiperSlide>
-        </Swiper>
-    </Flex>
-  )
+          {slides.map((slide, index) => (
+            <SwiperSlide virtualIndex={index}> 
+                <Flex 
+                w='100%'
+                h='100%'
+                align='center' 
+                justify='center' 
+                bgImage={`url(${slide.figure})`}
+                bgRepeat='no-repeat'
+                bgPosition='center'
+                bgSize='cover'
+                textAlign='center'
+                >
+                <Link href={slide.href}>
+                  <a>
+                    <Heading fontWeight='700' fontSize={['2xl', '3xl', '4xl', '5xl']} color='gray.50'> {slide.title} </Heading>
+                    <Text fontWeight='700' fontSize={['sm', 'lg', 'xl', '2xl']} color='gray.50'> {slide.description} </Text>
+                  </a>
+                </Link>
+              </Flex>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+      </Flex>
+    )
 }
